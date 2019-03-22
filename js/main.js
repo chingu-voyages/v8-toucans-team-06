a79fe7f4f2e4c33df522b661e6909ba213aed37c
Phil Curran <pecurran@hotmail.com>
2019-03-21 23:20:07 -0700

    tweaks and improvements to js

--- js/main.js
+++ js/main.js
@@ -1,19 +1,26 @@
 var url = "https://cors-anywhere.herokuapp.com/https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en&json=?";
-
+var quoteField = document.getElementById("quote");
+var authorField = document.getElementById("author");
+var quoteButton = document.getElementById("quoteButton");
 function getQuote() {
   fetch(url)
     .then(function(response) {
       return response.json();
     })
     .then(function(data) {
-      document.getElementById("quote").innerHTML = '"' + data.quoteText + '"';
-      if (data.quoteAuthor === "" || data.quoteAuthor == null) {
-        document.getElementById("author").innerHTML = "- Anonymous";
+      var quote = data.quoteText;
+      var author = data.quoteAuthor;
+      quoteField.innerHTML = '"' + quote + '"';
+      if (author === "" || author == null) {
+        authorField.innerHTML = "- Anonymous";
       } else {
-        document.getElementById("author").innerHTML = data.quoteAuthor;
+        authorField.innerHTML = "- " + data.quoteAuthor;
       }
     })
     .catch(function() {
-      document.getElementById("quote").innerHTML = "An error occured.  Please try again.";
+      quoteField.innerHTML = "An error occured.  Please try again.";
+      authorField.innerHTML = "";
     });
 };
+window.onload = getQuote();
+quoteButton.addEventListener("click", getQuote);
